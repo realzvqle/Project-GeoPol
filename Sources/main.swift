5bf07539f58fe93c510669c451c211d13b6e5109
realzvqle <realzvqle@gmail.com>
2024-12-11 18:19:37 -0600

    camera

--- Sources/main.swift
+++ Sources/main.swift
@@ -7,6 +7,8 @@ func main(){
     Raylib.initWindow(1600, 900, "Project Geopol")
     let player: Player = Player()
     let npc = NPC(index: 0)
+    let npc1 = NPC(index: 1)
+
     var camera = Camera2D(offset: Vector2.init(x: 0, y: 0), target: Vector2.init(x: 10, y: 10), rotation: 0, zoom: 1)
     while !Raylib.windowShouldClose {
         Raylib.beginDrawing()
@@ -15,6 +17,7 @@ func main(){
         Raylib.beginMode2D(camera)
         player.controlLoop(camera: &camera)
         npc.controlLoop(player: player)
+        npc1.controlLoop(player: player)
         Raylib.endMode2D()
         Raylib.endDrawing()
     }
